d9115765090383aab4d4af46cf5b24918c2e50a2
rtokkie <natto.and.eggs@gmail.com>
2022-09-18 13:15:10 +0900

    add chakra-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,17 @@
+import { Box, Container } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
 export const App = () => {
-  return <div>App</div>;
+  return <AppLayout>App</AppLayout>;
+};
+
+type AppLayoutProps = {
+  children: ReactNode;
+};
+const AppLayout = (props: AppLayoutProps) => {
+  return (
+    <Box h="full" bg="gray.50">
+      <Container>{props.children}</Container>
+    </Box>
+  );
 };
