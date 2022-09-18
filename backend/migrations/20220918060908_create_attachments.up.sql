fe4c23fa5e2dc4c0f105281748785534d549dd6f
rtokkie <natto.and.eggs@gmail.com>
2022-09-18 21:51:34 +0900

    a

--- backend/migrations/20220918060908_create_attachments.up.sql
+++ backend/migrations/20220918060908_create_attachments.up.sql
@@ -3,6 +3,6 @@ create table attachments (
   record_type varchar not null,
   record_id varchar not null,
   record_name varchar not null,
-  create_at timestamptz not null,
+  created_at timestamptz not null,
   blob_id int references attachments(id) on delete cascade
 );
--- backend/src/infrastructure/storage_service_impl.rs
+++ backend/src/infrastructure/storage_service_impl.rs
@@ -3,7 +3,7 @@ use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use derive_new::new;
 use serde::{Deserialize, Serialize};
-use sqlx::{query, PgPool};
+use sqlx::{query, query_as, PgPool};
 use std::sync::Arc;
 
 #[derive(new, Debug, Clone)]
@@ -14,13 +14,54 @@ pub struct StorageServiceImpl {
 #[async_trait]
 impl StorageService for StorageServiceImpl {
     async fn insert(&self, attachment: Attachment, blob: Blob) -> Result<(), Error> {
+        let blob = query_as!(
+            BlobRow,
+            r#"
+insert into blobs
+(file_data, filename, content_type, byte_size, metadata, created_at)
+values
+($1, $2, $3, $4, $5, $6)
+returning *
+            "#,
+            blob.file_data,
+            blob.filename,
+            blob.content_type,
+            blob.byte_size,
+            blob.metadata,
+            blob.created_at
+        )
+        .fetch_one(&*self.pool)
+        .await?;
+
+        query!(
+            r#"
+insert into attachments
+(record_type, record_id, record_name, created_at, blob_id)
+values
+($1, $2, $3, $4, $5)
+            "#,
+            attachment.record_type,
+            attachment.record_id,
+            attachment.record_name,
+            attachment.created_at,
+            blob.id
+        )
+        .execute(&*self.pool)
+        .await?;
+
         Ok(())
     }
 }
 
+impl From<sqlx::Error> for Error {
+    fn from(e: sqlx::Error) -> Self {
+        Error::Unknown(format!("{:?}", e))
+    }
+}
+
 #[derive(new, Debug, Serialize, Deserialize)]
-pub struct AttachmentRow {
-    pub id: i64,
+struct AttachmentRow {
+    pub id: i32,
     pub record_type: String,
     pub record_id: String,
     pub record_name: String,
@@ -29,8 +70,8 @@ pub struct AttachmentRow {
 }
 
 #[derive(new, Debug, Serialize, Deserialize)]
-pub struct BlobRow {
-    pub id: i64,
+struct BlobRow {
+    pub id: i32,
     pub file_data: Vec<u8>,
     pub filename: String,
     pub content_type: String,
